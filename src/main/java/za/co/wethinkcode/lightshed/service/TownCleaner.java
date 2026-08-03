7462ec404ccb3636d944be60f80b8efa6614d65a
ntxolscc-g-025 <ntxolscc-g-025@student.wethinkcode.co.za>
2026-08-03 13:29:19 +0200

    test: added failing tests for TownCleaner (RED)

--- src/main/java/za/co/wethinkcode/lightshed/service/TownCleaner.java
+++ src/main/java/za/co/wethinkcode/lightshed/service/TownCleaner.java
@@ -1,4 +1,9 @@
 package za.co.wethinkcode.lightshed.service;
 
 public class TownCleaner {
+
+    public String cleanText(String inputText){
+
+        return "NOT_CLEANED_YET";
+    }
 }
--- src/test/java/za/co/wethinkcode/lightshed/TownCleanerTest.java
+++ src/test/java/za/co/wethinkcode/lightshed/TownCleanerTest.java
@@ -1,4 +1,33 @@
 package za.co.wethinkcode.lightshed;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.lightshed.service.TownCleaner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TownCleanerTest {
+    private TownCleaner townCleaner;
+
+    @BeforeEach
+    void setUp(){
+        townCleaner = new TownCleaner();
+    }
+    @Test
+    void testTrimsSpaceAndFormatsCapitalization(){
+        assertEquals("Goerge", townCleaner.cleanText(" george "));
+    }
+    @Test
+    void testHandlesMultiWordTown(){
+        assertEquals("cape town", townCleaner.cleanText("   CAPE  TOWN  "));
+    }
+    @Test
+    void testHandlesMixedCaseInput(){
+        assertEquals("johannesburg", townCleaner.cleanText("jOhAnNeSbUrG"));
+    }
+    @Test
+    void testHandlesNullBlankInput(){
+        assertEquals("", townCleaner.cleanText(null));
+        assertEquals("", townCleaner.cleanText(" "));
+    }
 }
