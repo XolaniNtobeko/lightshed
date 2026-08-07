6d2f6728dd617471ec2e4dd54c23a43e0078f803
ntxolscc-g-025 <ntxolscc-g-025@student.wethinkcode.co.za>
2026-08-07 15:16:28 +0200

    feat: added failing integration test for GET /api/tonws/{name} (RED)

--- src/main/java/za/co/wethinkcode/lightshed/PlaceNameServer.java
+++ src/main/java/za/co/wethinkcode/lightshed/PlaceNameServer.java
@@ -1,4 +1,22 @@
 package za.co.wethinkcode.lightshed;
 
+import io.javalin.Javalin;
+import za.co.wethinkcode.lightshed.service.TownRepository;
+
 public class PlaceNameServer {
+
+    private Javalin app;
+    private TownRepository repository;
+
+    public PlaceNameServer(TownRepository repository){
+        this.repository = repository;
+        this.app = Javalin.create();
+    }
+
+    public Javalin getApp(){
+        return this.app;
+    }
+    public void start(int port){
+        this.app.start(port);
+    }
 }
--- src/test/java/za/co/wethinkcode/lightshed/PlaceNameServerTest.java
+++ src/test/java/za/co/wethinkcode/lightshed/PlaceNameServerTest.java
@@ -1,4 +1,39 @@
 package za.co.wethinkcode.lightshed;
 
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.lightshed.service.TownCleaner;
+import za.co.wethinkcode.lightshed.service.TownRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlaceNameServerTest {
+    private Javalin app;
+    @BeforeEach
+    void setUp(){
+        TownCleaner cleaner = new TownCleaner();
+        TownRepository repository = new TownRepository(cleaner);
+        repository.loadFromCsv("town.csv");
+
+        PlaceNameServer server= new PlaceNameServer(repository);
+        app = server.getApp();
+    }
+    @Test
+    void testGetTownByNameReturns200AndTown(){
+        JavalinTest.test(app, ((server, client) -> {
+            var response = client.get("/api/towns/George");
+
+            assertEquals(200, response.code());
+            assertTrue(response.body().string().contains("George"));
+        }));
+    }
+    @Test
+    void testGetTownByNameReturns404WhenNotFound(){
+        JavalinTest.test(app, ((server, client) -> {
+            var response = client.get("/api/towns/UnkownTown");
+            assertEquals(404, response.code());;
+        }));
+    }
 }
