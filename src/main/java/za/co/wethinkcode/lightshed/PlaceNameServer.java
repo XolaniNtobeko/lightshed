2913b8a695871d6e80816026d002e62b3c523c70
ntxolscc-g-025 <ntxolscc-g-025@student.wethinkcode.co.za>
2026-08-07 15:38:01 +0200

    feat: implemented GET /api/towns/{name} endpoint (GREEN)

--- src/main/java/za/co/wethinkcode/lightshed/PlaceNameServer.java
+++ src/main/java/za/co/wethinkcode/lightshed/PlaceNameServer.java
@@ -1,16 +1,46 @@
 package za.co.wethinkcode.lightshed;
 
+import com.sun.source.tree.BreakTree;
 import io.javalin.Javalin;
 import za.co.wethinkcode.lightshed.service.TownRepository;
 
+import java.util.List;
+
 public class PlaceNameServer {
 
-    private Javalin app;
-    private TownRepository repository;
+    private final Javalin app;
+    private final TownRepository repository;
 
     public PlaceNameServer(TownRepository repository){
         this.repository = repository;
         this.app = Javalin.create();
+
+        setupRoutes();
+    }
+
+    public void setupRoutes(){
+        // GET /api/towns (all or filtered)
+        app.get("/api/towns", ctx -> {
+            String name = ctx.queryParam("name");
+            if(name != null && !name.isBlank()){
+                ctx.json(repository.findByName(name));
+            }
+            else{
+                ctx.json(repository.getAllTowns());
+            }
+        });
+        // GET /api/towns/{name} (single town lookup)
+        app.get("/api/towns/{name}", ctx -> {
+            String townName = ctx.pathParam("name");
+            List matchings = repository.findByName(townName);
+
+            if(matchings.isEmpty()){
+                ctx.status(404);
+            }
+            else{
+                ctx.json(matchings.get(0));
+            }
+        });
     }
 
     public Javalin getApp(){
